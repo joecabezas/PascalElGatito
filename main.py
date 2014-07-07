7d36b4ffb9c892949e536a1526edc588782a39e5
Joe Cabezas <joe.cabezas@gmail.com>
2014-07-06 22:53:58 -0400

    encapsulation added into a class named TwitterCam

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ from twitter_data import APP_SECRET
 from twitter_data import OAUTH_TOKEN
 from twitter_data import OAUTH_TOKEN_SECRET
 
-#create twython object
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+class TwitterCam(object):
+	def __init__(self, a_animated):
+		self.twitter_obj = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+		self.animated = a_animated
+	
+	@property
+	def command(self):
+		command = 'sh take_picture.sh'.split()
+		if self.animated:
+			command.append('-a')
+		return command
 
-#is this take will be animated? (gif)
-animated = False
+	def start(self):
 
-#create command
-command = 'sh take_picture.sh'.split()
-if animated:
-	command.append('-a')
-#print command
+		#excecute command
+		outputFileName = subprocess.check_output(self.command)
+		outputFileName = outputFileName.strip()
 
-#excecute command
-outputFileName = subprocess.check_output(command)
-outputFileName = outputFileName.strip()
+		#tweet
+		ouputFile = open(outputFileName, 'rb')
+		twitter.update_status_with_media(status='Testing '+str(animated), media=ouputFile)
 
-#tweet
-ouputFile = open(outputFileName, 'rb')
-twitter.update_status_with_media(status='Testing '+str(animated), media=ouputFile)
+		print "done!"
 
-print "done!"
+	def test(self):
+		print self.command;
+		#twitter.update_status(status='Test debug')
